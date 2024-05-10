98ef92399a17d73a43cdeed89061cf08525897d9
KhangTranNhat <121271732+KhangTranNhat@users.noreply.github.com>
2024-05-10 13:08:21 +0700

    Delete BE directory

--- BE/src/main/java/ecom/Models/USER_ROLE.java
+++
@@ -1,9 +0,0 @@
-package ecom.Models;
-
-public enum USER_ROLE {
-ROLE_CUSTOMER,
-
-ROLE_RESTAURANT_OWNER,
-
-ADMIN
-}
--- BE/src/main/java/ecom/Repositories/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package ecom.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ecom.Models.CartModel;
-
-public interface CartRepository extends JpaRepository<CartModel, Integer> {
-    
-}
